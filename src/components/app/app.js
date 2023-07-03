80e631efc9b6f07f0fd0fa45004a10fe9ef8c5a1
xulgorak <xulgorak@gmail.com>
2023-07-03 14:24:00 +0300

    Добавление поиска и фильтров по кнопкам

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,9 @@ class App extends Component {
         {name: 'John C.', salary: 800, rise: false, increase: false, id: 1},
         {name: 'Alex M.', salary: 3000, rise: false, increase: false, id: 2},
         {name: 'Carl W.', salary: 5000, rise: false, increase: false, id: 3}
-      ]
+      ],
+      term: '',
+      filter: 'all'
     }
     this.maxId = 4
   }
@@ -82,10 +84,40 @@ onToggleProp = (id, prop) => {
   }))
 }
 
-  render() {
+searchEmp = (items, term) => {
+  if (term.length === 0) return items
 
+  return items.filter(item => {
+    return item.name.indexOf(term) > -1
+  })
+}
+
+onUpdateSearch = (term) => {
+  this.setState({term})
+}
+
+filterPost = (items, filter) => {
+  switch (filter) {
+    case 'rise':
+      return items.filter(item => item.rise)
+    case 'moreThenThousand':
+      return items.filter(item => item.salary > 1000)
+    default: 
+      return items
+  }
+}
+
+onFilterSelect = (filter) => {
+  this.setState({filter})
+}
+
+
+
+  render() {
+    const {data, term, filter} = this.state
     const employees = this.state.data.length;
     const increased = this.state.data.filter(item => item.increase).length
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter)
 
     return (
       <div className="app">
@@ -93,12 +125,12 @@ onToggleProp = (id, prop) => {
                    increased={increased}/>
   
           <div className="search-panel">
-              <SearchPanel/>
-              <AppFilter/>
+              <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+              <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
           </div>
           
           <EmployeesList 
-              data={this.state.data} 
+              data={visibleData} 
               onDelete = {this.deleteItem}
               onToggleProp={this.onToggleProp}/>
           <EmployeesAddForm
