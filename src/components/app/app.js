272439feaf54c94bee7412931f8ec321f5046cac
xulgorak <xulgorak@gmail.com>
2023-07-03 13:39:47 +0300

    Добавление валидации практика подьема состояния

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +7,105 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [
+        {name: 'John C.', salary: 800, rise: false, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, rise: false, increase: false, id: 2},
+        {name: 'Carl W.', salary: 5000, rise: false, increase: false, id: 3}
+      ]
+    }
+    this.maxId = 4
+  }
 
-  const data = [
-    {name: 'John C.', salary: 800, increase: false, id: 1},
-    {name: 'Alex M.', salary: 3000, increase: false, id: 2},
-    {name: 'Carl W.', salary: 5000, increase: false, id: 3}
-  ]
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+        // const index = data.findIndex(elem => elem.id === id) 
+        // const before = data.slice(0, index)
+        // const after = data.slice(index + 1)
+        // const newArr = [...before, ...after]
 
-  return (
-    <div className="app">
-        <AppInfo />
+        return {
+          data: data.filter(item => item.id !== id)
+        }
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList data={data} />
-        <EmployeesAddForm />
-    </div>
-  );
+    })
+  }
+
+
+  addItem = (name, salary) => {
+
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      rise: false,
+      id: this.maxId++
+    }
+
+    this.setState(({data}) => {
+
+      const newData = [...data, newItem]
+
+      return {
+        data: newData
+      }
+    })
+  }
+
+
+
+
+onToggleProp = (id, prop) => {
+  // this.setState(({data}) => {
+  //   // const index = data.findIndex(elem => elem.id === id)
+
+  //   // const old = data[index]
+  //   // const newItem = {...old, increase: !old.increase}
+
+  //   // const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)]
+
+  //   // return {
+  //   //   data: newArr
+  //   // }
+  // })
+
+  this.setState(({data}) => ({
+    data: data.map(item => {
+      if (item.id === id) {
+        return {...item, [prop]: !item[prop]}
+      }
+      return item;
+    })
+  }))
+}
+
+  render() {
+
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter(item => item.increase).length
+
+    return (
+      <div className="app">
+          <AppInfo employees={employees}
+                   increased={increased}/>
+  
+          <div className="search-panel">
+              <SearchPanel/>
+              <AppFilter/>
+          </div>
+          
+          <EmployeesList 
+              data={this.state.data} 
+              onDelete = {this.deleteItem}
+              onToggleProp={this.onToggleProp}/>
+          <EmployeesAddForm
+              onAdd = {this.addItem} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -17,7 +17,15 @@ class EmployeesAddForm extends Component {
         })
     }
 
-
+    onSubmit = (e) => {
+        e.preventDefault()
+        if (this.state.name < 3 || !this.state.salary) return;
+        this.props.onAdd(this.state.name, this.state.salary)
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
     
     render() {
 
@@ -27,7 +35,8 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?" 
